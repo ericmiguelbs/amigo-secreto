b2e2104a4de564d1fddb5f467585eb5a93aa9d35
Eric Miguel <ericmiguelbdasilva.com@gmail.com>
2025-03-02 10:49:17 -0300

    amigo secreto

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,45 @@
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.getElementById("amigo");
+    let nome = amigo.value.trim();
+
+    if (nome == "") {
+        alert("Por favor, insira um nome");
+        return;
+    }
+
+    listaDeAmigos.push(nome); 
+    listaDeAmigos.sort(); 
+ 
+    limpar();
+    exibirLista()
+}
+
+function limpar() {
+    let nome = document.querySelector("input");
+    nome.value = "";
+}
+
+function sortearAmigo() {
+    if (listaDeAmigos.length < 2) {  
+        alert("Não tem amigos para sortear");
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+        let amigoSorteado = listaDeAmigos[indiceAleatorio];
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `O amigo secreto é: <strong>${amigoSorteado}</strong>`;
+    }
+}
+
+function exibirLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = listaDeAmigos[i]; 
+        lista.appendChild(li);
+    }
+}
+
